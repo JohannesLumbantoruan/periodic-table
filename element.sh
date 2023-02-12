ada9014cee2e601017633fa576d021c02e4c8abf
JohannesLumbantoruan <Johannestrikardo@gmail.com>
2023-02-12 17:03:44 +0000

    fix: Fixing some problems in element.sh

--- element.sh
+++ element.sh
@@ -26,10 +26,11 @@ else
     echo "I could not find that element in the database."
   else
     ELEMENT_INFO=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-    echo $ELEMENT_INFO | while read ATOM_NUMBER BAR TYPE BAR MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+    echo $ELEMENT_INFO | while read ATOM_NUMBER BAR MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
     do
       ELEMENT_NAME=$($PSQL "SELECT name FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $ATOMIC_NUMBER")
       ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $ATOMIC_NUMBER")
+      TYPE=$($PSQL "SELECT DISTINCT(type) FROM types INNER JOIN properties USING(type_id) WHERE type_id = $TYPE_ID")
       echo "The element with atomic number $ATOM_NUMBER is $(echo $ELEMENT_NAME | sed -r 's/^ *//g' ) ($(echo $ELEMENT_SYMBOL | sed -r 's/^ *//g' )). It's a $(echo $TYPE | sed -r 's/^ *//g' ), with a mass of $(echo $MASS | sed -r 's/^ *//g' ) amu. $(echo $ELEMENT_NAME | sed -r 's/^ *//g' ) has a melting point of $(echo $MELTING_POINT | sed -r 's/^ *//g' ) celsius and a boiling point of $(echo $BOILING_POINT | sed -r 's/^ *//g' ) celsius."
     done
   fi
